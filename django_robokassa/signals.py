0861807990535d0f8ef397c2d247ea69dcd4be51
des <salavatsharapov@gmail.com>
2023-01-16 11:12:53 +0500

    Conformity with Django 4

--- django_robokassa/signals.py
+++ django_robokassa/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
-result_received = Signal(providing_args=["InvId", "OutSum"])
-success_page_visited = Signal(providing_args=["InvId", "OutSum"])
-fail_page_visited = Signal(providing_args=["InvId", "OutSum"])
+result_received = Signal()
+success_page_visited = Signal()
+fail_page_visited = Signal()
